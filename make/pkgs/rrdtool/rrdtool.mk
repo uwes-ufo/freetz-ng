1e4f2cdd214bfe45c5266a7ef9fd20655563addd
fda77 <fda77@users.noreply.github.com>
2026-08-08 08:44:19 +0200

    rrdtool: fix rpath

--- make/pkgs/rrdtool/rrdtool.mk
+++ make/pkgs/rrdtool/rrdtool.mk
@@ -24,11 +24,11 @@ $(PKG)_LIBS_BUILD_DIR:=$($(PKG)_LIBS_SELECTED:%=$($(PKG)_DIR)/src/.libs/%)
 $(PKG)_LIBS_STAGING_DIR:=$($(PKG)_LIBS_SELECTED:%=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/%)
 $(PKG)_LIBS_TARGET_DIR:=$($(PKG)_LIBS_SELECTED:%=$($(PKG)_TARGET_LIBDIR)/%)
 
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
+
 ifeq ($(strip $(FREETZ_LIB_librrd_WITH_VERSION_ABANDON)),y)
 $(PKG)_DEPENDS_ON += config-host
 $(PKG)_DEPENDS_ON += libpng freetype libart_lgpl zlib
-
-$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_UPDATE_CONFIGS,./)
 
