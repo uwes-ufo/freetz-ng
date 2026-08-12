179a9e99a160e1a023cd8e98f2142af534274e3c
fda77 <fda77@users.noreply.github.com>
2026-08-12 02:39:46 +0200

    siproxd: aarch64 support

--- make/pkgs/siproxd/siproxd.mk
+++ make/pkgs/siproxd/siproxd.mk
@@ -19,7 +19,9 @@ $(PKG)_EXCLUDED += $(patsubst %,$($(PKG)_DEST_DIR)$($(PKG)_MODULES_DIR)/plugin_%
 $(PKG)_EXCLUDED += $(patsubst %,$($(PKG)_DEST_DIR)$($(PKG)_MODULES_DIR)/plugin_%.la,$(filter-out $($(PKG)_MODULES),$($(PKG)_MODULES_ALL)))
 
 $(PKG)_DEPENDS_ON += libosip2 libtool
+$(PKG)_DEPENDS_ON += config-host
 
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_UPDATE_CONFIGS,./scripts)
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 
 # use system libltdl, not the bundled one
