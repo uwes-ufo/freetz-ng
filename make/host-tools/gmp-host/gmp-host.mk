f650a7801a4cea47904c463b411b1f8d7cd90c6c
fda77 <fda77@users.noreply.github.com>
2026-08-14 11:07:18 +0200

    gmp: update comments

--- make/host-tools/gmp-host/gmp-host.mk
+++ make/host-tools/gmp-host/gmp-host.mk
@@ -1,4 +1,5 @@
 $(call TOOLS_INIT, 6.3.0)
+# Update libs/ too !
 $(PKG)_SOURCE:=gmp-$($(PKG)_VERSION).tar.xz
 $(PKG)_HASH:=a3c2b80201b89e68616f4ad30bc66aee4927c3ce50e33929ca819d5c43538898
 $(PKG)_SITE:=@GNU/gmp
