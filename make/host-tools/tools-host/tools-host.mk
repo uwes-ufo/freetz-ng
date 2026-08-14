3ed0adfd3790ddd6db479b7f600e9ecd6b0dc8d8
fda77 <fda77@users.noreply.github.com>
2026-08-14 16:35:44 +0200

    bump tools-host

--- make/host-tools/tools-host/tools-host.mk
+++ make/host-tools/tools-host/tools-host.mk
@@ -1,6 +1,6 @@
-$(call TOOLS_INIT, 2026-08-13)
+$(call TOOLS_INIT, 2026-08-14)
 $(PKG)_SOURCE:=tools-$($(PKG)_VERSION).tar.xz
-$(PKG)_HASH:=c5147e7621c80f63da8c57b893f9fcad8ab0b6d659cc64eaf8a0953f2d7902a6
+$(PKG)_HASH:=9e9343307b9a0bc3fde4811042c809e8ac6de2846006ab86fa4535ab5c492c44
 $(PKG)_SITE:=@DLTOKEN/https://api.github.com/repos/Freetz-NG/internal/releases/tags/host-tools
 ### STEWARD:=fda77
 
